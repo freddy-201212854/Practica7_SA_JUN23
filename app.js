0ca5d84790699d550b0d0d7503b62c9560a6e6bd
freddy-201212854 <freddy.a.monterroso@gmail.com>
2023-06-22 21:07:19 -0600

    cambio de url

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 // Conectar a MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/goDB', {
+mongoose.connect('mongodb://mongo:27017/goDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
